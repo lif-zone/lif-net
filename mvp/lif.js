7de832f28db9aa9941058c9d5e2e5b65959facc0
lif-rnd <lif.zone.main@gmail.com>
2022-08-22 18:48:47 +0300

    mvp http

--- mvp/lif.js
+++ mvp/lif.js
@@ -1,5 +1,6 @@
 // author: derry. coder: arik.
 'use strict'; /*jslint node:true, browser:true*/
+import LBuffer from '../peer-relay/lbuffer.js';
 /* XXX WIP
 import LIF;
 
@@ -17,11 +18,31 @@ let host = yield dns.find({
   yield dns.declare({dns_record: {domain: 'lif.zone', host: me}});
 
 let derry_img = xxx_get_byte_arry_of_derry_jpeg
-let scroll = new LIF.Scroll({scroll: {pub_key, topic: 'http', domain: 'derry.lif.zone'}, decl: {ts}});
+let scroll = new LIF.Scroll(
+  {scroll: {pub_key, topic: 'http', domain: 'derry.lif.zone'}, decl: {ts}});
 // XXX derry: if {decl: {ts}} is missing, auto-add it in api (unless opt.no_ts)
 // XXX derry: why need decl prefix (just use ts)
 // XXX derry: why need http_record, just use http
-scroll.decl({http_record: {uri: '/', mime_type: 'text/html'}, decl: {ts});
-scroll.decl({http_record: {uri: '/derry.jpg', mime_type: 'image/jpeg'}, decl: {ts}, derry_img);
+scroll.decl({http_record: {uri: '/', mime_type: 'text/html'}, decl: {ts}});
+scroll.decl({http_record: {uri: '/derry.jpg', mime_type: 'image/jpeg'},
+  decl: {ts}}, derry_img);
 
 */
+
+const E = {};
+export default E;
+
+class Scroll {
+  constructor(opt){
+    this.lines = [];
+    this.keys = opt.keys;
+  }
+  decl(o){
+    let l = new LBuffer();
+    Array.from(arguments).forEach(o=>l.add_tail_json(o));
+    l.sign({keys: this.keys});
+    this.lines.push(l);
+  }
+}
+
+E.Scroll = Scroll;
--- mvp/main.js
+++ mvp/main.js
@@ -1,11 +1,22 @@
 // author: derry. coder: arik.
-import etask from '../util/etask.js';
 import https_server from '../lib/https_server.js';
+import LIF from './lif.js';
+const cwd = process.cwd();
 
 async function start(){
   console.log('MVP Start');
   https_server.start();
-//  return etask.wait();
+  let app = https_server.app;
+  app.get('/_', debug_handler);
+  app.get('/pub/debug.bundle.js',
+    (req, res)=>res.sendFile(cwd+'/pub/debug.bundle.js'));
+}
+
+function debug_handler(req, res){
+  res.send(`<html>
+    <script src=pub/debug.bundle.js></script>
+    <body><div id=root\></body>
+  <html>`);
 }
 
 function exit(msg){

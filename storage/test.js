dacf4d7a4ec57e5c30db43c94fe94bb1029b47e4
lif-rnd <lif.zone.main@gmail.com>
2022-09-05 08:59:12 +0300

    storage test

---
+++ storage/test.js
@@ -0,0 +1,27 @@
+'use strict'; /*jslint node:true*/ /*global describe,it,beforeEach,afterEach*/
+// XXX: need jslint mocha: true
+import assert from 'assert';
+import xutil from '../util/util.js';
+import xerr from '../util/xerr.js';
+
+// XXX: make it automatic for all node/browser in proc.js
+xerr.set_exception_catch_all(true);
+process.on('uncaughtException', err=>xerr.xexit(err));
+process.on('unhandledRejection', err=>xerr.xexit(err));
+xerr.set_exception_handler('test', (prefix, o, err)=>xerr.xexit(err));
+
+if (!xutil.is_inspect())
+  beforeEach(function(){ xerr.set_buffered(true, 1000); });
+
+afterEach(function(){
+  if (this.currentTest.timedOut){
+    xerr.notice(this.currentTest.err.stack);
+    assert.fail(this.currentTest.fullTitle()+': FAILED TIMEOUT');
+  }
+  xerr.clear();
+  xerr.set_buffered(false);
+});
+
+describe('basic', ()=>{
+  it('test', ()=>{});
+});

ff0c78e7c00f3093674dbf8bbcf96ba4e4a2d2ee
lif-rnd <lif.zone.main@gmail.com>
2022-12-19 17:24:37 +0200

    coding

--- util/proc.js
+++ util/proc.js
@@ -7,21 +7,19 @@ const E = {}, env = process.env;
 export default E;
 
 E.xexit_init = ()=>{
-    function xexit_on_err(err){ xerr.xexit(err); }
-    xerr.on_exception = function(err){
-        if (!(err instanceof TypeError || err instanceof ReferenceError
-            || err instanceof assert.AssertionError))
-        {
-            return;
-        }
-        if (env.ZEXIT_ON_TYPEERROR===undefined || +env.ZEXIT_ON_TYPEERROR)
-            return xexit_on_err(err);
-        if (err.sent_perr)
-            return;
-        err.sent_perr = true;
-        console.error('etask_typeerror '+err);
-    };
-    if (!xutil.is_mocha())
-        process.on('uncaughtException', xexit_on_err);
+  function xexit_on_err(err){ xerr.xexit(err); }
+  xerr.on_exception = function(err){
+    if (!(err instanceof TypeError || err instanceof ReferenceError
+        || err instanceof assert.AssertionError)){
+        return;
+    }
+    if (env.ZEXIT_ON_TYPEERROR===undefined || +env.ZEXIT_ON_TYPEERROR)
+      return xexit_on_err(err);
+    if (err.sent_perr)
+      return;
+    err.sent_perr = true;
+    console.error('etask_typeerror '+err);
+  };
+  if (!xutil.is_mocha())
+    process.on('uncaughtException', xexit_on_err);
 };
-

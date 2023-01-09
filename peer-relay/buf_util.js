3903ffb093255edc98f1aa7c214503b0470abb38
lif-rnd <lif.zone.main@gmail.com>
2023-01-09 18:47:37 +0200

    coding

--- peer-relay/buf_util.js
+++ peer-relay/buf_util.js
@@ -5,6 +5,24 @@ import {Buffer} from 'buffer';
 const E = {};
 export default E;
 
-E.buf_to_str = function(b){ return b ? b.toString('hex') : ''; };
-E.buf_from_str = function(s){ return Buffer.from(s, 'hex'); };
+E.buf_to_str = E.b2s = function(b){ return b ? b.toString('hex') : ''; };
+E.buf_from_str = E.s2b = function(s){ return Buffer.from(s, 'hex'); };
 E.buf_eq = function(a, b){ return a && b ? a.equals(b) : !a && !b; };
+E.b2s_obj = function(o, ret){ // XXX: need test
+  if (!o || !(o instanceof Object))
+    return o;
+  ret = ret||{};
+  for (let name in o){
+    let v = o[name];
+    if (v instanceof Uint8Array)
+      ret[name] = E.b2s(Buffer.from(v));
+    else if (Buffer.isBuffer(v))
+      ret[name] = E.b2s(v);
+    else if (v instanceof Object){
+      ret[name] = {};
+      E.b2s_obj(v, ret[name]);
+    } else
+      ret[name] = v;
+  }
+  return ret;
+};

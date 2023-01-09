24e44f15240eca539216f5e0991d07a5d59061b3
lif-rnd <lif.zone.main@gmail.com>
2023-01-09 12:39:48 +0200

    don't allow errors in emit_async

--- util/events_async.js
+++ util/events_async.js
@@ -1,20 +1,22 @@
 // author: derry. coder: arik.
 import {EventEmitter} from 'events';
 import etask from './etask.js';
+import xerr from './xerr.js';
 
+// XXX: need test
 export default class EventEmitterAsync extends EventEmitter {
   emit_async(){
-    let _this = this, args = Array.from(arguments), e = args.shift();
-    let listeners = this._events[e];
-    if (!listeners)
-      return;
-    // XXX: verify EventEmitter and mimic same behavior when adding/removing
-    // listeners during call and also handle erros the same
-    if (!Array.isArray(listeners))
-      return listeners.apply(this, args);
-    return etask(function*emit_async(){
-      for (let i=0; i<listeners.length; i++)
-        yield listeners[i].apply(_this, args);
-    });
+    try {
+      let _this = this, args = Array.from(arguments), e = args.shift();
+      let events = this._events[e];
+      if (!events)
+        return;
+      if (!Array.isArray(events))
+        return events.apply(this, args);
+      return etask(function*emit_async(){
+        for (let i=0; i<events.length; i++)
+          yield events[i].apply(_this, args);
+      });
+    } catch(err){ xerr.xexit(err); }
   }
 }

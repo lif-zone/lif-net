d75df0a0bb4a74d0b445de6ed6d50bbdd8c154a3
lif-rnd <lif.zone.main@gmail.com>
2023-01-09 12:32:38 +0200

    cleanup

--- storage/scroll.js
+++ storage/scroll.js
@@ -1,7 +1,7 @@
 // author: derry. coder: arik.
 'use strict';
 import assert from 'assert';
-import {EventEmitter} from 'events';
+import EventEmitterAsync from '../util/events_async.js';
 import crypto from '../util/crypto.js';
 import util from '../util/util.js';
 import etask from '../util/etask.js';
@@ -28,24 +28,6 @@ function to_frame(o){ // XXX: need test
   if (typeof o=='string')
     return {buf: Buffer.from(o)};
   assert.fail('invalid frame data '+o);
-}
-
-// XXX: mv to util + review with derry
-class EventEmitterAsync extends EventEmitter {
-  emit_async(){
-    let _this = this, args = Array.from(arguments), e = args.shift();
-    let listeners = this._events[e];
-    if (!listeners)
-      return;
-    // XXX: verify EventEmitter and mimic same behavior when adding/removing
-    // listeners during call and also handle erros the same
-    if (!Array.isArray(listeners))
-      return listeners.apply(this, args);
-    return etask(function*emit_async(){
-      for (let i=0; i<listeners.length; i++)
-        yield listeners[i].apply(_this, args);
-    });
-  }
 }
 
 class Data extends EventEmitterAsync {

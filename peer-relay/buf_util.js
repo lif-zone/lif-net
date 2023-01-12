91b401800e0ff98333a4a62ade3a5f44c651d078
lif-rnd <lif.zone.main@gmail.com>
2023-01-12 16:34:24 +0200

    fix b2s_obj

--- peer-relay/buf_util.js
+++ peer-relay/buf_util.js
@@ -11,6 +11,12 @@ E.buf_eq = function(a, b){ return a && b ? a.equals(b) : !a && !b; };
 E.b2s_obj = function(o, ret){ // XXX: need test
   if (!o || !(o instanceof Object))
     return o;
+  if (Array.isArray(o)){
+    ret = ret||[];
+    for (let i=0; i<o.length; i++)
+      ret[i] = E.b2s_obj(o[i]);
+    return ret;
+  }
   ret = ret||{};
   for (let name in o){
     let v = o[name];
@@ -18,6 +24,10 @@ E.b2s_obj = function(o, ret){ // XXX: need test
       ret[name] = E.b2s(Buffer.from(v));
     else if (Buffer.isBuffer(v))
       ret[name] = E.b2s(v);
+    else if (Array.isArray(v)){
+      ret[name] = [];
+      E.b2s_obj(v, ret[name]);
+    }
     else if (v instanceof Object){
       ret[name] = {};
       E.b2s_obj(v, ret[name]);

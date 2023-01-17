c8e9322cd55b61c1db9ccb38a9d17b5bae37cfd7
lif-rnd <lif.zone.main@gmail.com>
2023-01-17 12:34:30 +0200

    coding

--- util/util.js
+++ util/util.js
@@ -15,11 +15,11 @@ E.is_inspect = function(){
 
 E._is_mocha = undefined;
 E.is_mocha = function(){
-    if (E._is_mocha!==undefined)
-        return E._is_mocha;
-    E._is_mocha = ['afterEach', 'after', 'beforeEach', 'before', 'describe',
-      'it'].every(function(name){ return global[name] instanceof Function; });
+  if (E._is_mocha!==undefined)
     return E._is_mocha;
+  E._is_mocha = ['afterEach', 'after', 'beforeEach', 'before', 'describe',
+    'it'].every(function(name){ return global[name] instanceof Function; });
+  return E._is_mocha;
 };
 
 E.is_lxc = function(){ return is_node && +process.env.LXC; };
@@ -42,171 +42,153 @@ E.range.ei = function(x, a, b){ return x>a && x<=b; };
 E.range.ee = function(x, a, b){ return x>a && x<b; };
 
 E.clamp = function(lower_bound, value, upper_bound){
-    if (value < lower_bound)
-        return lower_bound;
-    if (value < upper_bound)
-        return value;
-    return upper_bound;
+  if (value < lower_bound)
+    return lower_bound;
+  if (value < upper_bound)
+    return value;
+  return upper_bound;
 };
 
-E.revcmp = function(a, b){
-    return a>b ? -1 : a<b ? 1 : 0; };
+E.revcmp = (a, b)=>a>b ? -1 : a<b ? 1 : 0;
 
 /* Union given objects, using fn to resolve conflicting keys */
 E.union_with = function(fn /* [o1, [o2, [...]]]*/){
-    var res = {}, args;
-    if (arguments.length==2 && typeof arguments[1]=='object')
-        args = arguments[1];
-    else
-        args = Array.prototype.slice.call(arguments, 1);
-    for (var i = 0; i < args.length; ++i)
-    {
-        for (var key in args[i])
-        {
-            var arg = args[i];
-            // eslint-disable-next-line no-prototype-builtins
-            res[key] = res.hasOwnProperty(key) ? fn(res[key], arg[key])
-                : arg[key];
-        }
+  var res = {}, args;
+  if (arguments.length==2 && typeof arguments[1]=='object')
+    args = arguments[1];
+  else
+    args = Array.prototype.slice.call(arguments, 1);
+  for (var i = 0; i < args.length; ++i){
+    for (var key in args[i]){
+      var arg = args[i];
+      // eslint-disable-next-line no-prototype-builtins
+      res[key] = res.hasOwnProperty(key) ? fn(res[key], arg[key]) : arg[key];
     }
-    return res;
+  }
+  return res;
 };
 
 function _clone_deep(obj){
-    var i, n, ret;
-    if (obj instanceof Array)
-    {
-        ret = new Array(obj.length);
-        n = obj.length;
-        for (i = 0; i < n; i++)
-            ret[i] = obj[i] instanceof Object ? _clone_deep(obj[i]): obj[i];
-        return ret;
-    }
-    else if (obj instanceof Date)
-        return new Date(obj);
-    else if (obj instanceof RegExp)
-        return new RegExp(obj);
-    else if (obj instanceof Function)
-        return obj;
-    ret = {};
-    for (i in obj)
-        ret[i] = obj[i] instanceof Object ? _clone_deep(obj[i]) : obj[i];
+  var i, n, ret;
+  if (obj instanceof Array){
+    ret = new Array(obj.length);
+    n = obj.length;
+    for (i = 0; i < n; i++)
+      ret[i] = obj[i] instanceof Object ? _clone_deep(obj[i]): obj[i];
     return ret;
+  }
+  else if (obj instanceof Date)
+    return new Date(obj);
+  else if (obj instanceof RegExp)
+    return new RegExp(obj);
+  else if (obj instanceof Function)
+    return obj;
+  ret = {};
+  for (i in obj)
+    ret[i] = obj[i] instanceof Object ? _clone_deep(obj[i]) : obj[i];
+  return ret;
 }
 
 E.clone_deep = function(obj){
-    if (!(obj instanceof Object))
-        return obj;
-    return _clone_deep(obj);
+  if (!(obj instanceof Object))
+    return obj;
+  return _clone_deep(obj);
 };
 
 // prefer to normally Object.assign() instead of extend()
 E.extend = function(obj){ // like _.extend
-    for (var i=1; i<arguments.length; i++)
-    {
-        var source = arguments[i];
-        if (!source)
-            continue;
-        for (var prop in source)
-            obj[prop] = source[prop];
-    }
-    return obj;
+  for (var i=1; i<arguments.length; i++){
+    var source = arguments[i];
+    if (!source)
+      continue;
+    for (var prop in source)
+      obj[prop] = source[prop];
+  }
+  return obj;
 };
 
-function is_object(obj){
-    return obj && obj.constructor==Object; }
+function is_object(obj){ return obj && obj.constructor==Object; }
 
 E.extend_deep = function(obj){
-    if (!is_object(obj))
-        return obj;
-    for (var i=1; i<arguments.length; i++)
-    {
-        var source = arguments[i];
-        if (!source)
-            continue;
-        for (var prop in source)
-        {
-            if (is_object(source[prop]) && is_object(obj[prop]))
-                E.extend_deep(obj[prop], source[prop]);
-            else
-                obj[prop] = source[prop];
-        }
-    }
+  if (!is_object(obj))
     return obj;
+  for (var i=1; i<arguments.length; i++){
+    var source = arguments[i];
+    if (!source)
+      continue;
+    for (var prop in source){
+      if (is_object(source[prop]) && is_object(obj[prop]))
+        E.extend_deep(obj[prop], source[prop]);
+      else
+        obj[prop] = source[prop];
+    }
+  }
+  return obj;
 };
 E.extend_deep_del_null = function(obj){
-    for (var i=1; i<arguments.length; i++)
-    {
-        var source = arguments[i];
-        if (!source)
-            continue;
-        for (var prop in source)
-        {
-            if (is_object(source[prop]))
-            {
-                if (!is_object(obj[prop]))
-                    obj[prop] = {};
-                E.extend_deep_del_null(obj[prop], source[prop]);
-            }
-            else if (source[prop]==null)
-                delete obj[prop];
-            else
-                obj[prop] = source[prop];
-        }
+  for (var i=1; i<arguments.length; i++){
+    var source = arguments[i];
+    if (!source)
+      continue;
+    for (var prop in source){
+      if (is_object(source[prop])){
+        if (!is_object(obj[prop]))
+          obj[prop] = {};
+        E.extend_deep_del_null(obj[prop], source[prop]);
+      }
+      else if (source[prop]==null)
+        delete obj[prop];
+      else
+        obj[prop] = source[prop];
     }
-    return obj;
+  }
+  return obj;
 };
 
 E.defaults = function(obj){ // like _.defaults
-    if (!obj)
-        obj = {};
-    for (var i=1; i<arguments.length; i++)
-    {
-        var source = arguments[i];
-        if (obj===undefined)
-            continue;
-        for (var prop in source)
-        {
-            if (obj[prop]===undefined)
-                obj[prop] = source[prop];
-        }
+  if (!obj)
+    obj = {};
+  for (var i=1; i<arguments.length; i++){
+    var source = arguments[i];
+    if (obj===undefined)
+      continue;
+    for (var prop in source){
+      if (obj[prop]===undefined)
+        obj[prop] = source[prop];
     }
-    return obj;
+  }
+  return obj;
 };
 E.defaults_deep = function(obj){
-    if (obj!==undefined && !is_object(obj))
-        return obj;
-    for (var i=1; i<arguments.length; i++)
-    {
-        var source = arguments[i];
-        if (obj===undefined)
-            obj = E.clone_deep(source);
-        else if (is_object(source))
-        {
-            for (var prop in source)
-            {
-                var s = source[prop], d = obj[prop];
-                if (d===undefined)
-                    obj[prop] = E.clone_deep(s);
-                else
-                    E.defaults_deep(d, s);
-            }
-        }
-    }
+  if (obj!==undefined && !is_object(obj))
     return obj;
+  for (var i=1; i<arguments.length; i++){
+    var source = arguments[i];
+    if (obj===undefined)
+      obj = E.clone_deep(source);
+    else if (is_object(source)){
+      for (var prop in source){
+        var s = source[prop], d = obj[prop];
+        if (d===undefined)
+          obj[prop] = E.clone_deep(s);
+        else
+          E.defaults_deep(d, s);
+      }
+    }
+  }
+  return obj;
 };
 
 E.clone = function(obj){ // like _.clone
-    if (!(obj instanceof Object))
-        return obj;
-    if (obj instanceof Array)
-    {
-        var a = new Array(obj.length);
-        for (var i=0; i<obj.length; i++)
-            a[i] = obj[i];
-        return a;
-    }
-    return E.extend({}, obj);
+  if (!(obj instanceof Object))
+    return obj;
+  if (obj instanceof Array){
+    var a = new Array(obj.length);
+    for (var i=0; i<obj.length; i++)
+      a[i] = obj[i];
+    return a;
+  }
+  return E.extend({}, obj);
 };
 
 // Limitations:
@@ -216,260 +198,238 @@ E.clone = function(obj){ // like _.clone
 // string serializations, which can lead to conflation of distinct closures.
 // Cyclic references are not supported (cause a stack overflow).
 E.equal_deep = function(a, b){
-    var i;
-    if (a===b)
-        return true;
-    if (!a || !b || a.constructor!==b.constructor)
-        return false;
-    if (a instanceof Function || a instanceof RegExp)
-        return a.toString()==b.toString();
-    if (a instanceof Date)
-        return +a == +b;
-    if (Array.isArray(a))
-    {
-        if (a.length!=b.length)
-            return false;
-        for (i = 0; i<a.length; i++)
-        {
-            if (!E.equal_deep(a[i], b[i]))
-                return false;
-        }
-        return true;
-    }
-    if (is_object(a))
-    {
-        var a_keys = Object.keys(a), b_keys = Object.keys(b);
-        if (a_keys.length!=b_keys.length)
-            return false;
-        for (i = 0; i<a_keys.length; i++)
-        {
-            var key = a_keys[i];
-            if (!E.equal_deep(a[key], b[key]))
-                return false;
-        }
-        return true;
-    }
+  var i;
+  if (a===b)
+    return true;
+  if (!a || !b || a.constructor!==b.constructor)
     return false;
+  if (a instanceof Function || a instanceof RegExp)
+    return a.toString()==b.toString();
+  if (a instanceof Date)
+    return +a == +b;
+  if (Array.isArray(a)){
+    if (a.length!=b.length)
+      return false;
+    for (i = 0; i<a.length; i++){
+      if (!E.equal_deep(a[i], b[i]))
+          return false;
+    }
+    return true;
+  }
+  if (is_object(a)){
+    var a_keys = Object.keys(a), b_keys = Object.keys(b);
+    if (a_keys.length!=b_keys.length)
+      return false;
+    for (i = 0; i<a_keys.length; i++){
+      var key = a_keys[i];
+      if (!E.equal_deep(a[key], b[key]))
+          return false;
+    }
+    return true;
+  }
+  return false;
 };
 
 // like _.map() except returns object, not array
 E.map_obj = function(obj, fn){
-    var ret = {};
-    for (var i in obj)
-        ret[i] = fn(obj[i], i, obj);
-    return ret;
+  var ret = {};
+  for (var i in obj)
+    ret[i] = fn(obj[i], i, obj);
+  return ret;
 };
 
 // recursivelly recreate objects with keys added in order
 E.sort_obj = function(obj, fn){
-    if (obj instanceof Array || !(obj instanceof Object))
-        return obj;
-    var ret = {}, keys = Object.keys(obj).sort(fn);
-    for (var i=0; i<keys.length; i++)
-        ret[keys[i]] = E.sort_obj(obj[keys[i]], fn);
-    return ret;
+  if (obj instanceof Array || !(obj instanceof Object))
+    return obj;
+  var ret = {}, keys = Object.keys(obj).sort(fn);
+  for (var i=0; i<keys.length; i++)
+    ret[keys[i]] = E.sort_obj(obj[keys[i]], fn);
+  return ret;
 };
 
 // an Object equivalent of Array.prototype.forEach
 E.forEach = function(obj, fn, _this){
-    for (var i in obj)
-        fn.call(_this, obj[i], i, obj);
+  for (var i in obj)
+    fn.call(_this, obj[i], i, obj);
 };
 // an Object equivalent of Array.prototype.find
 E.find = function(obj, fn, _this){
-    for (var i in obj)
-    {
-        if (fn.call(_this, obj[i], i, obj))
-            return obj[i];
-    }
+  for (var i in obj){
+    if (fn.call(_this, obj[i], i, obj))
+        return obj[i];
+  }
 };
-E.find_prop = function(obj, prop, val){
-    return E.find(obj, function(o){ return o[prop]===val; }); };
+E.find_prop = (obj, prop, val)=>E.find(obj,
+  function(o){ return o[prop]===val; });
 E.isspace = function(c){ return /\s/.test(c); };
 E.isdigit = function(c){ return c>='0' && c<='9'; };
 E.isalpha = function(c){ return c>='a'&&c<='z' || c>='A'&&c<='Z'; };
 E.isalnum = function(c){ return E.isdigit(c)||E.isalpha(c); };
 
 E.obj_pluck = function(obj, prop){
-    var val = obj[prop];
-    delete obj[prop];
-    return val;
+  var val = obj[prop];
+  delete obj[prop];
+  return val;
 };
 
 // Object.keys() does not work on prototype
 E.proto_keys = function(proto){
-    var keys = [];
-    for (var i in proto)
-        keys.push(i);
-    return keys;
+  var keys = [];
+  for (var i in proto)
+    keys.push(i);
+  return keys;
 };
 
 E.values = function(obj){
-    var values = [];
-    for (var i in obj)
-        values.push(obj[i]);
-    return values;
+  var values = [];
+  for (var i in obj)
+    values.push(obj[i]);
+  return values;
 };
 
 E.path = function(path){
-    if (Array.isArray(path))
-        return path;
-    path = ''+path;
-    if (!path)
-        return [];
-    return path.split('.');
+  if (Array.isArray(path))
+    return path;
+  path = ''+path;
+  if (!path)
+    return [];
+  return path.split('.');
 };
 E.get = function(o, path, def){
-    path = E.path(path);
-    for (var i=0; i<path.length; i++)
-    {
-        if (!o || typeof o!='object'&&typeof o!='function' || !(path[i] in o))
-            return def;
-        o = o[path[i]];
-    }
-    return o;
+  path = E.path(path);
+  for (var i=0; i<path.length; i++){
+    if (!o || typeof o!='object'&&typeof o!='function' || !(path[i] in o))
+      return def;
+    o = o[path[i]];
+  }
+  return o;
 };
 E.set = function(o, path, value){
-    var orig = o;
-    path = E.path(path);
-    for (var i=0; i<path.length-1; i++)
-    {
-        var p = path[i];
-        o = o[p] || (o[p] = {});
-    }
-    o[path[path.length-1]] = value;
-    return orig;
+  var orig = o;
+  path = E.path(path);
+  for (var i=0; i<path.length-1; i++){
+    var p = path[i];
+    o = o[p] || (o[p] = {});
+  }
+  o[path[path.length-1]] = value;
+  return orig;
 };
 E.unset = function(o, path){
-    path = E.path(path);
-    for (var i=0; i<path.length-1; i++)
-    {
-        var p = path[i];
-        if (!o[p])
-            return;
-        o = o[p];
-    }
-    delete o[path[path.length-1]];
+  path = E.path(path);
+  for (var i=0; i<path.length-1; i++){
+    var p = path[i];
+    if (!o[p])
+      return;
+    o = o[p];
+  }
+  delete o[path[path.length-1]];
 };
 var has_unique = {};
-E.has = function(o, path){ return E.get(o, path, has_unique)!==has_unique; };
-E.own = function(o, prop){
-    return Object.prototype.hasOwnProperty.call(o, prop); };
+E.has = (o, path)=>E.get(o, path, has_unique)!==has_unique;
+E.own = (o, prop)=>Object.prototype.hasOwnProperty.call(o, prop);
 
 E.bool_lookup = function(a, split){
-    var ret = {}, i;
-    if (typeof a=='string')
-        a = a.split(split||/\s/);
-    for (i=0; i<a.length; i++)
-        ret[a[i]] = true;
-    return ret;
+  var ret = {}, i;
+  if (typeof a=='string')
+    a = a.split(split||/\s/);
+  for (i=0; i<a.length; i++)
+    ret[a[i]] = true;
+  return ret;
 };
 
 E.clone_inplace = function(dst, src){
-    if (dst===src)
-        return dst;
-    if (Array.isArray(dst))
-    {
-        for (var i=0; i<src.length; i++)
-            dst[i] = src[i];
-        dst.splice(src.length);
-    }
-    else if (typeof dst=='object')
-    {
-        var k;
-        for (k in src)
-            dst[k] = src[k];
-        for (k in dst)
-        {
-            // eslint-disable-next-line no-prototype-builtins
-            if (!src.hasOwnProperty(k))
-                delete dst[k];
-        }
-    }
+  if (dst===src)
     return dst;
+  if (Array.isArray(dst)){
+    for (var i=0; i<src.length; i++)
+      dst[i] = src[i];
+    dst.splice(src.length);
+  }
+  else if (typeof dst=='object'){
+    var k;
+    for (k in src)
+      dst[k] = src[k];
+    for (k in dst){
+      // eslint-disable-next-line no-prototype-builtins
+      if (!src.hasOwnProperty(k))
+          delete dst[k];
+    }
+  }
+  return dst;
 };
 
 if (node_util && node_util.inherits)
-    E.inherits = node_util.inherits;
-else
-{
-    // implementation from node.js 'util' module
-    E.inherits = function inherits(ctor, superCtor){
-        ctor.super_ = superCtor;
-        ctor.prototype = Object.create(superCtor.prototype,
-            {constructor: {value: ctor, enumerable: false, writable: true,
-            configurable: true}});
-    };
+  E.inherits = node_util.inherits;
+else {
+  // implementation from node.js 'util' module
+  E.inherits = function inherits(ctor, superCtor){
+    ctor.super_ = superCtor;
+    ctor.prototype = Object.create(superCtor.prototype,
+      {constructor: {value: ctor, enumerable: false, writable: true,
+      configurable: true}});
+  };
 }
 
 // ctor must only have one prototype level
 E.inherit_init = function(obj, ctor, params){
-    var orig_proto = Object.getPrototypeOf(obj);
-    var ctor_proto = Object.assign({}, ctor.prototype);
-    Object.setPrototypeOf(ctor_proto, orig_proto);
-    Object.setPrototypeOf(obj, ctor_proto);
-    return ctor.apply(obj, params);
+  var orig_proto = Object.getPrototypeOf(obj);
+  var ctor_proto = Object.assign({}, ctor.prototype);
+  Object.setPrototypeOf(ctor_proto, orig_proto);
+  Object.setPrototypeOf(obj, ctor_proto);
+  return ctor.apply(obj, params);
 };
 
 E.pick = function(obj){
-    var i, o = {};
-    for (i=1; i<arguments.length; i++)
-    {
-        if (E.own(obj, arguments[i]))
-            o[arguments[i]] = obj[arguments[i]];
-    }
-    return o;
+  var i, o = {};
+  for (i=1; i<arguments.length; i++){
+    if (E.own(obj, arguments[i]))
+        o[arguments[i]] = obj[arguments[i]];
+  }
+  return o;
 };
 
 // subset of _.omit
 E.omit = function(obj, omit){
-    var i, o = {};
-    obj = Object(obj);
-    for (i in obj)
-    {
-        if (!omit.includes(i))
-            o[i] = obj[i];
-    }
-    return o;
+  var i, o = {};
+  obj = Object(obj);
+  for (i in obj){
+    if (!omit.includes(i))
+      o[i] = obj[i];
+  }
+  return o;
 };
 
 E.if_set = function(val, o, name){
-    if (val!==undefined)
-        o[name] = val;
+  if (val!==undefined)
+    o[name] = val;
 };
 
 E.escape_dotted_keys = function(obj, repl){
-    if (!Array.isArray(obj) && !is_object(obj))
-        return obj;
-    repl = repl||'_';
-    for (var prop in obj)
-    {
-        if (E.own(obj, prop))
-        {
-            var new_prop = prop.replace(/\./g, repl);
-            if (prop != new_prop)
-            {
-                obj[new_prop] = obj[prop];
-                delete obj[prop];
-            }
-            if (Array.isArray(obj[new_prop]))
-            {
-                obj[new_prop].forEach(function(e){
-                    E.escape_dotted_keys(e, repl);
-                });
-            }
-            else if (is_object(obj[new_prop]))
-                E.escape_dotted_keys(obj[new_prop], repl);
-        }
+  if (!Array.isArray(obj) && !is_object(obj))
+    return obj;
+  repl = repl||'_';
+  for (var prop in obj){
+    if (E.own(obj, prop)){
+      var new_prop = prop.replace(/\./g, repl);
+      if (prop != new_prop){
+        obj[new_prop] = obj[prop];
+        delete obj[prop];
+      }
+      if (Array.isArray(obj[new_prop]))
+        obj[new_prop].forEach(function(e){ E.escape_dotted_keys(e, repl); });
+      else if (is_object(obj[new_prop]))
+        E.escape_dotted_keys(obj[new_prop], repl);
     }
+  }
 };
 
 E.ensure_array = function(v, split){
-    if (v==null || Array.isArray(v))
-        return v||[];
-    if (split && typeof v=='string')
-        return v.split(split==true ? /[ ,]+/ : split).filter(Boolean);
-    return [v];
+  if (v==null || Array.isArray(v))
+    return v||[];
+  if (split && typeof v=='string')
+    return v.split(split==true ? /[ ,]+/ : split).filter(Boolean);
+  return [v];
 };
 
 E.is_number = function(s){ return /^[0-9]+$/.test(s); };

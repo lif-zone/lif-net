01ae53ef05615e11cecf0728e0f0e7be9c7dad72
lif-rnd <lif.zone.main@gmail.com>
2023-01-23 09:47:54 +0200

    fs test

---
+++ fs/test.js
@@ -0,0 +1,10 @@
+'use strict'; /*eslint-env mocha*/
+import proc from '../util/proc.js';
+import xtest from '../util/test_lib.js'; // eslint-disable-line no-unused-vars
+
+proc.init();
+
+it('xxx', ()=>{
+  const t = ()=>{};
+  t('xxx1', ``);
+});

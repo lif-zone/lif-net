23596d0b28f172b6b046e06639fc2771e978d04f
lif-rnd <lif.zone.main@gmail.com>
2023-01-25 12:47:47 +0200

    fs support

--- fs/fs.js
+++ fs/fs.js
@@ -4,28 +4,96 @@ import Scroll from '../storage/scroll.js';
 import crypto from '../util/crypto.js';
 import etask from '../util/etask.js';
 import xerr from '../util/xerr.js';
+import assert from 'assert';
 import buf_util from '../net/buf_util.js';
+import DiffMatchAndPath from 'diff-match-patch';
+const Diff = new DiffMatchAndPath();
 const b2s = buf_util.buf_to_str;
 
 export default class FS extends Scroll {
   constructor(opt){
     super(opt);
     this.buf_hash_to_seq = new Map();
+    this.file_to_seq = new Map();
   }
+  // XXX: support cfid
+  add_dir(dir){ return this.decl({op: 'add', dir}); }
+  // XXX: support cfid
   add_file(file, buf){ return etask({_: this}, function*add_file(){
     // XXX: throw error if trying to add the same file twice
     let _this = this._;
-    if (!buf)
-      return _this.decl({op: 'add', file});
+    if (!buf){
+      let decl = yield _this.decl({op: 'add', file});
+      _this.file_to_seq.set(file, decl.seq);
+      return;
+    }
     let h = b2s(crypto.hash(_this.crypt, buf)); // XXX _this.hash
     let link = _this.buf_hash_to_seq.get(h);
-    if (link)
-      return _this.decl({link}, [{op: 'add', file}]);
-    let decl = yield _this.decl([{op: 'add', file}, buf]);
+    if (link){
+      let decl = _this.decl({link}, [{op: 'add', file}]);
+      _this.file_to_seq.set(file, decl.seq);
+      return;
+    }
+    let decl = yield _this.decl([{op: 'add', file, content: 1}, buf]);
+    _this.file_to_seq.set(file, decl.seq);
     _this.buf_hash_to_seq.set(h, decl.seq);
     return decl;
   }); }
-  add_dir(dir){ return this.decl({op: 'add', dir}); }
+  // XXX: support cfid
+  mod_file(file, buf){ return etask({_: this}, function*mod_file(){
+    // XXX: handle case of same buf
+    let _this = this._;
+    _this.on('uncaught', e=>xerr.xexit(e));
+    assert(buf, 'XXX TODO empty file'); // XXX
+    let h = b2s(crypto.hash(_this.crypt, buf)); // XXX _this.hash
+    let link = _this.buf_hash_to_seq.get(h);
+    if (link){
+      let decl = _this.decl({link}, [{op: 'mod', file}]);
+      _this.file_to_seq.set(file, decl.seq);
+      return;
+    }
+    link = _this.file_to_seq.get(file);
+    if (!link)
+      throw new Error('file not found '+file);
+    let _buf = yield _this.resolve_buf(0, link), decl;
+    // XXX: need create_patch/apply_patch
+    let _s = Buffer.from(_buf).toString();
+    let s = Buffer.from(buf).toString();
+    let diff = Buffer.from(Diff.patch_toText(Diff.patch_make(_s, s)));
+    if (diff.length < 0.5*buf.length)
+      decl = yield _this.decl({link}, [{op: 'mod', file, diff: 1}, diff]);
+    else
+      decl = yield _this.decl([{op: 'mod', file, content: 1}, buf]);
+    _this.file_to_seq.set(file, decl.seq);
+  }); }
+  resolve_buf(cfid, seq){ return etask({_: this}, function*resolve_buf(){
+    // XXX: verify we test every part of it
+    let _this = this._, decl = _this.get_decl(seq);
+    yield decl.load(cfid, {data: true});
+    let header = decl.get_header(cfid);
+    let body = decl.get_body(cfid);
+    // XXX: wrap with resolve_buf_single
+    let o = FS.parse_buf_ref(body.diff ? body.diff : body.content), buf;
+    if (!o)
+      throw new Error('missing conent seq'+decl.seq);
+    if (o.buf)
+      buf = o.buf;
+    else if (o.d)
+      buf = yield decl.get_buf(o.d+2);
+    else {
+      if (!o.l)
+        throw new Error('missing conent link seq'+decl.seq);
+      let seq2 = Scroll.resolve_link(header.link, o.l);
+      if (seq2>=seq, 'link can only point backwards seq'+seq);
+      buf = yield _this.resolve_buf(cfid, seq2);
+    }
+    if (!body.diff)
+      return buf;
+    let _seq = Scroll.resolve_link(header.link, '_');
+    let _buf = yield _this.resolve_buf(cfid, _seq);
+    return Buffer.from(Diff.patch_apply(Diff.patch_fromText(buf.toString()),
+      _buf.toString())[0]);
+  }); }
   test_get_seq(cfid, seq){
     let decl = this.get_decl(seq);
     let header = decl.get_header(cfid);
@@ -38,10 +106,14 @@ export default class FS extends Scroll {
       ret.dir = body.dir;
     if (body.file)
       ret.file = body.file;
-    if (f2)
-      ret.f2 = f2;
+    if (body.diff)
+      ret.diff = body.diff;
+    if (body.content)
+      ret.content = body.content;
     if (header.link)
       ret.link = header.link;
+    if (f2)
+      ret.f2 = f2;
     return ret;
   }
 }
@@ -59,5 +131,20 @@ function valid_dir(dir){ return dir[dir.length-1]=='/'; }
 // XXX: need test + improve
 function valid_file(dir){ return dir[dir.length-1]!='/'; }
 
+function parse_buf_ref(ref){
+  if (ref===undefined || ref===null)
+    return {l: '_'};
+  if (Number.isInteger(ref))
+    return {d: ref};
+  if (typeof ref=='string')
+    return {buf: Buffer.from(ref)};
+  if (Number.isInteger(ref.d))
+    return {d: ref.d};
+  if (typeof ref.d=='string')
+    return {l: ref.d};
+  assert.fail('invalid ref %o', ref);
+}
+
 FS.valid_dir = valid_dir;
 FS.valid_file = valid_file;
+FS.parse_buf_ref = parse_buf_ref;
--- fs/test.js
+++ fs/test.js
@@ -2,9 +2,12 @@
 import assert from 'assert';
 import xtest from '../util/test_lib.js';
 import etask from '../util/etask.js';
+import xerr from '../util/xerr.js';
 import {Buffer} from 'buffer';
 import FS from './fs.js';
 import tparser from '../storage/test_parser.js';
+import DiffMatchAndPath from 'diff-match-patch';
+const Diff = new DiffMatchAndPath();
 const {parse_get_next, parse_exp, parse_exp_arg, rm_parentesis} = tparser;
 import {test_run, test_run_register_hook, new_scroll, get_scroll, get_def,
   test_register_get_seq} from '../storage/test_cmd.js';
@@ -33,7 +36,7 @@ const cmd_add = t=>etask(function*cmd_add(){
       buf = t_buf[tt.r];
       assert(buf, 'buf not found '+tt.r);
     } else {
-      assert(!dir && !file, 'invalid arg '+tt.cmd+' in '+t.meta.s);
+      assert(!file && !dir, 'invalid arg '+tt.cmd+' in '+t.meta.s);
       if (FS.valid_dir(tt.cmd))
         dir = tt.cmd;
       else if (FS.valid_file(tt.cmd))
@@ -42,10 +45,30 @@ const cmd_add = t=>etask(function*cmd_add(){
         assert.fail('invalid file/dir '+tt.cmd);
     }
   }
+  assert(file||dir, 'missing file/dir');
   if (file)
     yield fs.add_file(file, buf);
   else
     yield fs.add_dir(dir);
+});
+
+const cmd_mod = t=>etask(function*cmd_mod(){
+  this.on('uncaught', e=>xerr.xexit(e)); // XXX: need xtest.etask
+  let name = t.ctx||get_def('left'), fs = get_scroll(name), file, buf;
+  assert(t.r, 'missing arg '+t.meta.s);
+  for (let curr=t.r, i=0; curr = parse_get_next(curr); i++){
+    let tt = parse_exp_arg(curr.exp);
+    if (/^buf/.test(tt.cmd)){
+      buf = t_buf[tt.r];
+      assert(buf, 'buf not found '+tt.r);
+    } else {
+      assert(!file, 'invalid arg '+tt.cmd+' in '+t.meta.s);
+      file = tt.cmd;
+      assert(FS.valid_file(file), 'invalid file '+file);
+    }
+  }
+  assert(FS.valid_file(file), 'missing file');
+  yield fs.mod_file(file, buf);
 });
 
 const cmd_buf = t=>etask(function*cmd_buf(){
@@ -63,13 +86,14 @@ const cmd_buf = t=>etask(function*cmd_buf(){
     default: assert.fail('invalid arg '+tt.cmd);
     }
   }
-  t_buf[name] = Buffer.from(val);
+  t_buf[name] = Buffer.from(val+'\n');
 });
 
 const test_run_single = (curr, o, step)=>etask(function*_test_run_single(){
   switch (o.cmd){
   case 'fs': yield cmd_fs(o); break;
   case 'add': yield cmd_add(o); break;
+  case 'mod': yield cmd_mod(o); break;
   case 'buf': yield cmd_buf(o); break;
   default: return false;
   }
@@ -83,14 +107,39 @@ const get_seq = s=>etask(function*get_seq(){
   for (let curr=s; curr = parse_get_next(curr);){
     let o = parse_exp(curr.exp);
     // XXX yield get_val(o.r);
-    if (o.l=='f2')
-      bo[o.l] = t_buf[o.r];
-    else
+    if (o.l=='f2'){
+      let oo = parse_exp(o.r), a;
+      switch (oo.cmd){
+      case 'diff':
+        a = oo.r.split(',');
+        assert(a.length==2, 'invalid diff '+o.r);
+        bo[o.l] = Buffer.from(Diff.patch_toText(
+          Diff.patch_make(t_buf[a[0]].toString(), t_buf[a[1]].toString())));
+        break;
+      default:
+        assert(t_buf[o.r], 'buf not found '+o.r);
+        bo[o.l] = t_buf[o.r];
+      }
+    } else
       bo[o.l] = o.r;
   }
   return bo;
 });
 test_register_get_seq(get_seq);
+
+describe('util', ()=>{
+  it('parse_buf_ref', ()=>{
+    const t = (val, exp)=>assert.deepEqual(FS.parse_buf_ref(val), exp);
+    t(null, {l: '_'});
+    t(undefined, {l: '_'});
+    t(0, {d: 0});
+    t(1, {d: 1});
+    t('', {buf: Buffer.from('')});
+    t('a', {buf: Buffer.from('a')});
+    t({d: 1}, {d: 1});
+    t({d: '_'}, {l: '_'});
+  });
+});
 
 describe('fs', ()=>{
   const t = (name, test)=>it(name, ()=>test_run(test));
@@ -104,20 +153,34 @@ describe('fs', ()=>{
     add(/d2/d2d/)  #seq6={op:add dir:/d2/d2d/}
     add(/d2/d2d2/) #seq7={op:add dir:/d2/d2d2/}`);
   describe('file', ()=>{
+    let b1, b2, b3, b = 'x'.repeat(68);
+    // XXX: test empty file
+    // XXX: test binary
+    // XXX: test branches+conflict
     // XXX: support buf(b:123 b2:1234)
     t('basic', `s..#seq buf(b1 val:0) buf(b2 val:1)
       s..fs           #seq0={} // XXX: todo
-      add(/f1 buf:b1) #seq1={op:add file:/f1 f2:b1}
-      add(/f2 buf:b2) #seq2={op:add file:/f2 f2:b2}`);
-    if (0) // XXX WIP
-    t('diff', `s..#seq buf(b1 val:0123456789) buf(b2 val:01234567890)
-      buf(bdiff val:xxx)
+      add(/f1 buf:b1) #seq1={op:add file:/f1 content:1 f2:b1}
+      add(/f2 buf:b2) #seq2={op:add file:/f2 content:1 f2:b2}`);
+    [b1, b2, b3] = [b+'x1', b+'x2', b+'x3'];
+    t('mod_same', `s..#seq buf(b val:b)
       s..fs           #seq0={} // XXX: todo
-      add(/f1 buf:b1) #seq1={op:add file:/f1 f2:b1}
-      mod(/f1 buf:b2) #seq2={op:add file:/f1 link:1 diff:1 f2:bdiff}`);
+      add(/f buf:b) #seq1={op:add file:/f content:1 f2:b}
+      mod(/f buf:b) #seq2={op:mod file:/f link:1}`);
+    t('mod_diff', `s..#seq
+      buf(b1 val:${b1}) buf(b2 val:${b2}) buf(b3 val:${b3})
+      s..fs           #seq0={} // XXX: todo
+      add(/f1 buf:b1) #seq1={op:add file:/f1 content:1 f2:b1}
+      mod(/f1 buf:b2) #seq2={op:mod file:/f1 link:1 diff:1 f2:diff(b1,b2)}
+      mod(/f1 buf:b3) #seq3={op:mod file:/f1 link:2 diff:1 f2:diff(b2,b3)}`);
+    [b1, b2] = [b+'1', b+'2'];
+    t('mod_nodiff', `s..#seq buf(b1 val:${b1}) buf(b2 val:${b2})
+      s..fs           #seq0={} // XXX: todo
+      add(/f1 buf:b1) #seq1={op:add file:/f1 content:1 f2:b1}
+      mod(/f1 buf:b2) #seq2={op:mod file:/f1 content:1 f2:b2}`);
     t('link', `s..#seq buf(b1 val:0)
       s..fs           #seq0={} // XXX: todo
-      add(/f1 buf:b1) #seq1={op:add file:/f1 f2:b1}
+      add(/f1 buf:b1) #seq1={op:add file:/f1 content:1 f2:b1}
       add(/f2 buf:b1) #seq2={op:add file:/f2 link:1}`);
   });
   return;

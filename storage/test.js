273ed5f23016806877600e97ed2ce6d33d454096
lif-rnd <lif.zone.main@gmail.com>
2023-03-06 15:15:59 +0200

    coding

--- storage/test.js
+++ storage/test.js
@@ -2306,24 +2306,25 @@ describe('scroll', function(){
           s..decl({path:/derry})
           decl({path:$1}) $$(/derry /derry /derry /arik)
           S..#index scroll(s..M0)
-          tput(0 1                ) #index={id:0 key:/arik seq:1}
+          // XXX derry: I don't use macro here
+          tput(0 1                ) #index={id:0 key:/arik  seq:1}
           tput(0 1 2              ) #index={id:0 key:/derry seq:2}
-          tput(0 1 2 3            ) #index={id:0 key:/arik seq:3}
+          tput(0 1 2 3            ) #index={id:0 key:/arik  seq:3}
           tput(0 1 2 3 4          ) #index={id:0 key:/derry seq:4}
           tput(0 1 2 3 4 5        ) #index={id:0 key:/derry seq:5}
           tput(0 1 2 3 4 5 6      ) #index={id:0 key:/derry seq:6}
           tput(0 1 2 3 4 5 6 7    ) #index={id:0 key:/derry seq:7}
           tput(0 1 2 3 4 5 6 7 8  ) #index={id:0 key:/derry seq:8}
-          tput(0 1 2 3 4 5 6 7 8 9) #index={id:0 key:/arik seq:9}
-          tput(0 1 2 3 4 f        ) #index={id:1 key:/arik seq:5}
-          tput(0 1 2 3 4 f g      ) #index={id:1 key:/arik seq:6}
+          tput(0 1 2 3 4 5 6 7 8 9) #index={id:0 key:/arik  seq:9}
+          tput(0 1 2 3 4 f        ) #index={id:1 key:/arik  seq:5}
+          tput(0 1 2 3 4 f g      ) #index={id:1 key:/arik  seq:6}
           tput(0 1 2 3 4 f h      ) #index={id:1 key:/derry seq:7}
-          tput(0 1 2 3 4 f h i    ) #index={id:1 key:/arik seq:8}
-          tput(0 1 2 3 4 f h i j  ) #index={id:1 key:/arik seq:9}
+          tput(0 1 2 3 4 f h i    ) #index={id:1 key:/arik  seq:8}
+          tput(0 1 2 3 4 f h i j  ) #index={id:1 key:/arik  seq:9}
           tput(0 1 2 3 4 F        ) #index={id:2 key:/david seq:5}
           tput(0 1 2 3 4 F G      ) #index={id:2 key:/derry seq:6}
           tput(0 1 2 3 4 F G H    ) #index={id:2 key:/derry seq:7}
-          tput(0 1 2 3 4 F G H I  ) #index={id:2 key:/arik seq:8}
+          tput(0 1 2 3 4 F G H I  ) #index={id:2 key:/arik  seq:8}
           tput(0 1 2 3 4 F G H I J) #index={id:2 key:/derry seq:9}
           ##index_find(cfid:$1 name:path key:/arik bseq:$2)=$3 $$(
             (0 _10 [9       3 1]) // cfid:0

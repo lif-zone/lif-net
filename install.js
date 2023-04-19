06af343baed948e213bac915a17a7047a3c51dc1
lif-rnd <lif.zone.main@gmail.com>
2023-04-19 13:40:37 +0300

    installl WIP

--- install.js
+++ install.js
@@ -6,6 +6,7 @@ import xerr from './util/xerr.js';
 import proc from './util/proc.js';
 import etask from './util/etask.js';
 import string from './util/string.js';
+import date from './util/date.js';
 import url from './util/url.js';
 import {valid_file, valid_dir} from './fs/util.js';
 import {is_ipv4} from './util/net.js';
@@ -13,7 +14,7 @@ import prompt from 'prompt'; // XXX: fix vim coloring
 const {is_valid_domain} = url;
 const {split_ws} = string;
 const cwd = process.cwd();
-
+const ts = date();
 proc.xexit_init();
 
 // XXX: mv to util (and add fallback to other servers)
@@ -48,8 +49,16 @@ function is_no(s){ return /^no|n$/i.test(s.trim()); }
 
 function validate_yes_no(s){ return is_yes(s) || is_no(s); }
 
+function gen_conf(old, opt){
+  return {install_ts: opt.install_ts, ...old, production: true, ...opt};
+}
+
+function conf_str(conf){ return JSON.stringify(conf, null, '  '); }
+
 const main = ()=>etask(function*main(){
+  this.on('uncaught', err=>xerr.xexit(err));
   // XXX TODO: check preconditions (eg. node min version, free space)
+  // XXX: read prev configuration and allow to keep old settings
   console.log('Install LIF Server');
   let et_ip = get_my_ip();
   yield prompt.start(); // XXX: fix vim coloring and for default
@@ -74,7 +83,8 @@ const main = ()=>etask(function*main(){
   let src = cwd;
   let tmp = dst+'.tmp';
   let prev = dst+'.prev';
-  let conf_file = tmp+'/conf.json';
+  let tmp_conf_file = tmp+'/conf.json';
+  let tmp_id_file = tmp+'/install_id';
   xerr('XXX src %O', src);
   xerr('XXX dst %O', dst);
   xerr('XXX ip %O', ip);
@@ -98,17 +108,23 @@ const main = ()=>etask(function*main(){
   }
   console.log('Copy src to tmp dir %s', tmp);
   fs.cpSync(src, tmp, {force: true, recursive: true});
-  console.log('Create configuration file %s', conf_file);
-  let conf = yield import(conf_file, {assert: {type: 'json'}});
-  console.log('XXX %O', conf);
-  // XXX: update conf
+  fs.writeFileSync(tmp_id_file, date.to_sql_ms(ts));
+  console.log('Create configuration file %s', tmp_conf_file);
+  let conf = (yield import(tmp_conf_file, {assert: {type: 'json'}})).default;
+  let conf_new = gen_conf(conf, {install_ts: date.to_sql_ms(ts), ip, domain});
+  fs.writeFileSync(tmp_conf_file, conf_str(conf_new));
   // XXX: stop service
+  console.log('XXX WIP - stop service');
   if (need_prev){
-    // XXX: mv curr to prev
+    console.log('Remove old prev dir %s', prev);
+    fs.rmSync(prev, {recursive: true, force: true});
+    console.log('Save prev copy %s', prev);
+    fs.renameSync(dst, prev);
   }
-  // XXX: mv tmp to curr
-  console.log('Cleanup, rm tmp dir %s', tmp);
-  fs.rmSync(tmp, {recursive: true, force: true});
+  console.log('Move tmp dir to be new version %s', dst);
+  fs.renameSync(tmp, dst);
+  // XXX: start service
+  console.log('XXX WIP - start service');
   console.log();
 });
 

4e815a3c60a185a9b6871fb32d8a6cedb85e422b
lif-rnd <lif.zone.main@gmail.com>
2023-04-19 10:32:28 +0300

    install WIP

--- install.js
+++ install.js
@@ -3,11 +3,20 @@
 import xerr from './util/xerr.js';
 import proc from './util/proc.js';
 import etask from './util/etask.js';
+import prompt from 'prompt'; // XXX: fix vim coloring
 
 proc.xexit_init();
 
 const main = ()=>etask(function*main(){
-  xerr('XXX install');
+  console.log('Install LIF Server');
+  yield prompt.start(); // XXX: fix vim coloring
+  prompt.message = null;
+  let ret = yield prompt.get({name: 'dir', type: 'string', required: true,
+    default: '/var/lif/server', // XXX: fix vim coloring
+    description: 'Install dir',
+    validator: function(){ console.log('XXX %O', arguments); },
+    });
+  xerr('XXX result %O', ret);
 });
 
 main();

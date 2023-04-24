4af4d17d20cf71f999e0cc4437562a999300a753
lif-rnd <lif.zone.main@gmail.com>
2023-04-24 08:53:24 +0300

    acme WIP

--- net/acme.js
+++ net/acme.js
@@ -4,8 +4,10 @@ import ACME from 'acme';
 import etask from '../util/etask.js';
 import xerr from '../util/xerr.js';
 import util from '../util/util.js';
+// XXX: rm all @root dependencies
 import Keypairs from '@root/keypairs';
 import CSR from '@root/csr';
+import Enc from '@root/encoding';
 const {opt_array} = util;
 const E = {};
 export default E;
@@ -19,31 +21,28 @@ const acme_start = ()=>etask(function*acme_start(){
   this.on('uncaught', err=>xerr('XXX error %s', err.stack));
 // XXX: me email/agent to eneric place
   let acme = ACME.create({maintainerEmail: 'lif.zone.main@gmail.com',
-    packageAgent: 'lif/v0.0.1',
-    notify: notify_cb});
+    packageAgent: 'lif/v0.0.1', notify: notify_cb});
   // XXX: yield acme.init('https://acme-v02.api.letsencrypt.org/directory');
   yield acme.init('https://acme-staging-v02.api.letsencrypt.org/directory');
-  let key_pair = yield Keypairs.generate({kty: 'EC', format: 'jwk'});
-  let account_pair = yield Keypairs.generate({kty: 'EC', format: 'jwk'});
-  xerr('XXX key_pair %O', key_pair);
-  xerr('XXX account_pair %O', account_pair);
-  let csr = yield CSR.csr({jwk: key_pair.private, domains: ['lif.biz']});
+  let account_key = yield Keypairs.generate({kty: 'EC'}); // XXX: format: 'jwk'
+  let cert_key = yield Keypairs.generate({kty: 'EC'}); // XXX: format: 'jwk'
+  let csr = yield CSR.csr({jwk: cert_key.private, domains: ['lif.biz'],
+    encoding: 'der'});
+  csr = Enc.bufToUrlBase64(csr);
   xerr('XXX csr\n%s', csr);
-  yield acme.accounts.create({subscriberEmail: 'lif.zone.main@gmail.com',
-    agreeToTerms: true, accountKey: account_pair.private});
+  // XXX: get email from conf
+  let account = yield acme.accounts.create({agreeToTerms: true,
+    subscriberEmail: 'lif.zone.main@gmail.com',
+    accountKey: account_key.private});
   // XXX: https://letsencrypt.org/docs/challenge-types/#dns-01-challenge
-  let cert = yield acme.certificates.create({
-    account: account_pair.kid,
-    accountKey: account_pair.private,
-    serverKeypair: key_pair.private, // XXX: needed?
-    agreeToTerms: true,
-    csr,
-    domains: ['lif.biz'],
+  let cert = yield acme.certificates.create({account,
+    accountKey: account_key.private,
+    csr, customerEmail: null, domains: ['lif.biz'],
     // https://git.rootprojects.org/root/acme-dns-01-digitalocean.js/src/branch/master/lib/index.js
     challenges: {'dns-01': {
-      propagationDelay: 10,
+      propagationDelay: 5000,
       init: function(opts){
-        xerr('XXX request %O', opts.request);
+        xerr('XXX init %O', opts.request);
         return null;
       },
       zones: function(data){
@@ -51,7 +50,7 @@ const acme_start = ()=>etask(function*acme_start(){
         return ['lif.biz'];
       },
       get: opts=>{
-        xerr('XXX challenges get %s', JSON.stringify(opts, null, '  '));
+        xerr('XXX challenge_get %s', JSON.stringify(opts, null, '  '));
         return etask(function*(){
           yield etask.sleep(1);
           return true;
@@ -60,8 +59,8 @@ const acme_start = ()=>etask(function*acme_start(){
       set: opts=>{
         var ch = opts.challenge;
         var txt = ch.dnsAuthorization;
-        xerr('XXX challenges set %s', JSON.stringify(opts, null, '  '));
-        xerr('XXX challenges set dnsZone %s dnsPrefix %s data %s',
+        xerr('XXX challenges_set %s', JSON.stringify(opts, null, '  '));
+        xerr('XXX challenges_set dnsZone %s dnsPrefix %s data %s',
           ch.dnsZone, ch.dnsPrefix, txt);
         return etask(function*(){
           try {
@@ -76,18 +75,20 @@ const acme_start = ()=>etask(function*acme_start(){
       },
       remove: opts=>{
         var ch = opts.challenge;
-        xerr('XXX challenges remove %s', JSON.stringify(opts, null, '  '));
-        xerr('XXX challenges remove dnsZone %s dnsPrefix %s',
+        xerr('XXX SKIP challenges remove %s',
+          JSON.stringify(opts, null, '  '));
+        xerr('XXX SKIP challenges remove dnsZone %s dnsPrefix %s',
           ch.dnsZone, ch.dnsPrefix);
         return etask(function*(){
-          yield E.dnss.rm_txt(ch.dnsPrefix+'.'+ch.dnsZone);
           yield etask.sleep(1);
+          if (true) return; // XXX WIP
+          yield E.dnss.rm_txt(ch.dnsPrefix+'.'+ch.dnsZone);
           return true;
         });
       }
     }}
   });
-  xerr('XXX cert %O', cert);
+  xerr('XXX new cert %O', cert);
 });
 
 E.start = opt=>{

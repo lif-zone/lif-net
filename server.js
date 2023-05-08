3fc42725d6152d5e54e70d60abdb206d9575c38d
lif-rnd <lif.zone.main@gmail.com>
2023-05-08 09:02:01 +0300

    begin of git sync

--- server.js
+++ server.js
@@ -3,16 +3,30 @@
 import express from 'express';
 import http from 'http';
 import https from 'https';
+import fs from 'fs';
 import assert from 'assert';
 import Node from './net/node.js';
+import Scroll from './storage/scroll.js';
+import Soul from './storage/soul.js';
+import DB from './storage/db.js';
+import Storage_handler from './storage/storage.js';
+import Git from './fs/git.js';
 import dnss from './net/dnss.js';
 import ssl from './net/ssl.js';
 import etask from './util/etask.js';
 import xerr from './util/xerr.js';
 import proc from './util/proc.js';
 import conf from './util/conf.js';
+import crypto from './util/crypto.js';
+import browserify from 'browserify';
+import util from './util/util.js';
+import buf_util from './net/buf_util.js';
+const s2b = buf_util.buf_from_str, b2s = buf_util.buf_to_str;
+const {opt_array} = util;
 const cwd = process.cwd();
 const dir = conf.production ? conf.install_dir+'/server' : cwd;
+const build_dir = dir+'/build';
+let server_et;
 
 proc.xexit_init(do_exit);
 
@@ -72,7 +86,8 @@ function index_html_handler(req, res){ res.sendFile(dir+'/www/index.html'); }
 function sw_handler(req, res){ res.sendFile(dir+'/www/sw.js'); }
 
 // XXX: check caching/other headers
-function lif_node_handler(req, res){ res.sendFile(dir+'/www/lif_node.js'); }
+function lif_node_handler(req, res){
+  res.sendFile(build_dir+'/lif_node.bundle.js'); }
 
 // XXX: check caching/other headers
 function favicon_handler(req, res){ res.sendFile(dir+'/www/favicon.svg'); }
@@ -82,14 +97,122 @@ function babel_handler(req, res){
   res.sendFile(dir+'//node_modules/@babel/standalone/babel.js');
 }
 
-const start_lif_node = https_server=>etask(function*start_lif_node(){
+const lif_node_start = https_server=>etask(function*lif_node_start(){
   // XXX: save node id
   let node = new Node({https_server});
   xerr.notice('lif node id %s', node.id.s);
 });
 
+// XXX: mv to generic place
+const load_keypair = (file_key, file_pub)=>etask(function*load_keypair(){
+  let key, pub;
+  try { key = yield fs.promises.readFile(file_key, 'ascii'); }
+  catch(err){ return xerr('server: failed to load key %s', file_key); }
+  try { pub = yield fs.promises.readFile(file_pub, 'ascii'); }
+  catch(err){ return xerr('server: failed to load pub %s', file_pub); }
+  return {key: s2b(key), pub: s2b(pub)};
+});
+
+const soul_start = ()=>etask(function*soul_start(){
+  if (!conf.soul?.enable)
+    return xerr.notice('server: soul is disabled');
+  xerr.notice('server: soul is enabled');
+  let dir = conf.soul.dir||conf.dir+'/soul';
+  let file_key = dir+'/priv.key', file_pub = dir+'/pub.key';
+  let keypair = yield load_keypair(file_key, file_pub);
+  if (keypair)
+    xerr.notice('server: using keypair pub %s from %s', b2s(keypair.pub), dir);
+  else if (!keypair){
+    let crypt = Scroll.supported_crypt[0];
+    keypair = yield crypto.keypair(crypt);
+    xerr.notice('server: create new keypair pub %O at %s', b2s(keypair.pub),
+      dir);
+    yield fs.promises.writeFile(file_pub, b2s(keypair.pub).toString());
+    yield fs.promises.writeFile(file_key, b2s(keypair.key).toString());
+  }
+  if (!conf.soul.storage?.enable)
+    return xerr('server: storage is disabled');
+  let storage_dir = conf.soul.storage.dir||dir+'/storage';
+  xerr.notice('server: storage is enabled at %s', storage_dir);
+  yield DB.init({shim_conf: {checkOrigin: false, databaseBasePath: storage_dir,
+    useSQLiteIndexes: true}});
+  // XXX: need to save keypair in soul and a way to load/storre soul
+  let soul = new Soul({name: 'server'});
+  yield soul.db.init({postfix: soul.name});
+  if (!conf.soul.git?.enable)
+    return xerr('server: git is disabled');
+  let storage = new Storage_handler({db: soul.db});
+  let git_dir = conf.soul.git.dir||dir+'/git';
+  xerr.notice('server: git is enabled at %s', git_dir);
+  let src = 'https://github.com/lif-rnd/test_site';
+  let M, git;
+  if (!M){
+    git = yield Git.create({soul, ...keypair, storage}, {git: {src}});
+    M = git.name;
+    xerr.notice('server: git clone scroll %s src %s', git.name, src);
+  } else {
+    git = yield Git.open({M, soul, ...keypair, storage});
+    xerr.notice('server: git load scroll %s src %s', M, src);
+  }
+  xerr.notice('server: git sync %s src %s', M, src);
+  xerr.notice('server: sync START top %s top_oid %s', git.top.seq,
+    yield git.get_git_br_top_oid(0, 'main'));
+  yield git.sync({dir: git_dir+'/lif-rnd_test_site'});
+  yield git.flush(); // XXX: also do it on process exit
+  xerr.notice('server: sync DONE top %s top_oid %s', git.top.seq,
+    yield git.get_git_br_top_oid(0, 'main'));
+});
+
+// XXX: WIP
+const start_git = ()=>etask(function*start_git(){
+  if (!conf.git?.enable)
+    return xerr.notice('server: git clone is disabled');
+  let dir = conf.git.dir;
+  xerr.notice('server: git clone is enabled dir %s', dir);
+  let a = opt_array(conf.git.clone);
+  for (let i=0; i<a.length; i++){
+    let src = a[i];
+    xerr.notice('server: git clone %s', src);
+  }
+});
+
+const browserify_map = {};
+const browserify_js = (target, files, opt={})=>etask(function*browserify_js(){
+  // XXX: hack. need better memory map and also don't generate if in disk)
+  let key = target+JSON.stringify(opt);
+  if (browserify_map[key])
+    return this.wait_ext(browserify_map[key]);
+  let wait = browserify_map[key] = etask.wait();
+  xerr('browserfiy %s', target);
+  fs.mkdirSync(build_dir, {recursive: true});
+  var b = browserify(opt);
+  b.add(files);
+  let stream = fs.createWriteStream(target);
+  stream.on('close', ()=>{
+    xerr.notice('server: browserify %s DONE', target);
+    wait.continue();
+  });
+  stream.on('error', err=>{
+    xerr('server: browserify error %s', err);
+    wait.reject(err);
+  });
+  b.bundle().pipe(stream);
+  return wait;
+});
+
+function test_serve(test){
+  return (req, res)=>etask(function*(){
+    let file = test.replaceAll('/', '_').replace('.js', '.bundle.js');
+    yield browserify_js(build_dir+'/'+file,
+      [dir+'/'+test], {debug: true});
+    res.sendFile(build_dir+'/'+file);
+  });
+}
+
 const main = ()=>etask(function*main(){
+  assert(!server_et, 'server alredy running');
   this.on('uncaught', e=>xerr.xexit(e));
+  server_et = etask.wait();
   xerr.notice('run lif server %s cwd %s dir %s',
     conf.production ? 'PRODUCTION' : 'DEV', cwd, dir);
   assert(conf.ip, 'missing server ip, check conf.json');
@@ -100,14 +223,14 @@ const main = ()=>etask(function*main(){
   // XXX: need dynamic reload on src change
   // XXX: use link rel='modulepreload'
   let {app, https_server} = http_start(80, 443);
-  yield start_lif_node(https_server);
+  yield lif_node_start(https_server);
+  yield soul_start();
   app.use(function(req, res, next){
     // XXX: set CORS/caching
     res.setHeader('Service-Worker-Allowed', '/');
     next();
   });
   // XXX: rm in production
-  app.use('/.lif/src/', express.static(dir));
   app.get('/.lif/test.html', (req, res)=>res.sendFile(dir+'/www/test.html'));
   // XXX: fix test files to include mocha from local include
   app.get('/.lif/test_util.html',
@@ -118,22 +241,18 @@ const main = ()=>etask(function*main(){
     (req, res)=>res.sendFile(dir+'/www/test_net.html'));
   app.get('/.lif/test_fs.html',
     (req, res)=>res.sendFile(dir+'/www/test_fs.html'));
+  app.get('/.lif/build/util_test.bundle.js', test_serve('util/test.js'));
+  app.get('/.lif/build/net_test.bundle.js', test_serve('net/test.js'));
+  app.get('/.lif/build/storage_test.bundle.js', test_serve('storage/test.js'));
+  app.get('/.lif/build/fs_test.bundle.js', test_serve('fs/test.js'));
   app.get('/', index_html_handler);
   app.get('/.lif.sw.js', sw_handler);
-  app.get('/.lif/lif_node.js', lif_node_handler);
+  app.get('/.lif/lif_node.bundle.js', lif_node_handler);
   // XXX: review babel/favicon
   app.get('/.lif.babel.js', babel_handler);
   app.get('/.lif.favicon.svg', favicon_handler);
+  yield browserify_js(build_dir+'/lif_node.bundle.js', [dir+'/net/node.js'],
+    {debug: true, standalone: 'lif_node'});
 });
 
 main();
-
-/* XXX derry:;
-- review how to loader react modules
-- local development: google-chrome --ignore-certificate-errors
-- properly handle service-worker during load of service-worker + update
-- lif_compile: umd_to_es6
-- importScript in sw
-- expose all source code under / (it includes also conf file)
-- remove obsolete util/url.js
-*/

86d7855ca05a61ed11accdd3943fda9cb09fb2b4
lif-rnd <lif.zone.main@gmail.com>
2023-05-14 15:44:17 +0300

    add conf.js

---
+++ storage/conf.js
@@ -0,0 +1,10 @@
+// author: derry. coder: arik.
+'use strict';
+import Scroll from '../storage/scroll.js';
+
+// XXX: need test
+export default class Conf extends Scroll {
+}
+
+Conf.create = Scroll.create;
+Conf.open = Scroll.open;

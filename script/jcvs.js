cca485151a497b258c493bd8c54ac34a1584f66c
lif-rnd <lif.zone.main@gmail.com>
2024-04-30 10:39:48 +0300

    jcvs cvsup

--- script/jcvs.js
+++ script/jcvs.js
@@ -1,14 +1,25 @@
 #!/usr/bin/env node
 // author: derry. coder: arik.
+import etask from '../util/etask.js';
+import xerr from '../util/xerr.js';
+import proc from '../util/proc.js';
+import getopt from 'node-getopt';
+import {execSync} from 'node:child_process';
 
 /* XXX TODO
-jcvs co git@github.com:xarikgilad/home.git -d lif
+jcvs co git@github.com:xarikgilad/lif-zone-src.git -d lif
+jcvs co git@github.com:xarikgilad/home.git -d home
+jcvs co lif -d lif
+jcvs co home -d home
 
 # command line diff
 jcvs diff file/dir
 
 # update and show modified file status
 cvsup
+
+# update file/dir
+jcvs up file/dir
 
 # gvim diff
 cvsdiff file
@@ -30,12 +41,62 @@ zlint file/dir
 # gvim menu
 :CVSAnnotate
 
+# rgrep
+
 # add instructions for server debug
 # add instructions for web debug
+# rename xerr -> zerr
 */
 
+proc.xexit_init();
+let gopt = getopt.create([
+  ['d', 'd=directory', ''],
+  ]).bindHelp(
+    'Usage:\n'+
+    '  jcvs co repository -d [dir]\n'+
+    '  jcvs cvsup\n'+
+    '  cvsup\n'
+  ).parseSystem();
+
+function is_git(){
+  try { execSync('git status', {stdio: 'ignore'}); }
+  catch(err){ return false; }
+  return true;
+}
+
+function run_cvs(){
+  let a = Array.from(process.argv);
+  if (/\bnode$/.test(a[0]))
+    a.shift();
+  return execSync(a.join(' '));
+}
+
+function do_error(gopt, msg){
+  if (msg)
+    console.error(msg);
+  gopt.showHelp();
+  process.exit(1);
+}
+
+function git_cvsup(gopt, msg){
+  execSync('git pull');
+  let ret = execSync('git status -s');
+  console.log(ret.toString());
+}
 
 const main = ()=>etask(function*main(){
+  this.on('uncaught', e=>xerr.xexit(e));
+  let {argv, options} = gopt;
+  if (!is_git())
+    return run_cvs();
+  let cmd = argv[0];
+  switch (cmd){
+    case 'cvsup':
+      if (argv[1])
+        do_error(gopt, 'Invalid argument');
+      return git_cvsup();
+  default: do_error(gopt, 'Unknown command for GIT: '+cmd);
+  }
 });
 
 main();

6df2f6d0251eac6e2fc49741c9a3e4b0628822af
lif-rnd <lif.zone.main@gmail.com>
2024-04-30 09:11:10 +0300

    jcvs

---
+++ script/jcvs.js
@@ -0,0 +1,41 @@
+#!/usr/bin/env node
+// author: derry. coder: arik.
+
+/* XXX TODO
+jcvs co git@github.com:xarikgilad/home.git -d lif
+
+# command line diff
+jcvs diff file/dir
+
+# update and show modified file status
+cvsup
+
+# gvim diff
+cvsdiff file
+ -D "2 weeks ago"
+ -D "2024-10-13 13:52"
+
+# commit
+jcvs ci file/dir
+
+# add file/dir
+jcvs add file/dir
+
+# rm file/dir
+jcvs rm file/dir
+
+# lint
+zlint file/dir
+
+# gvim menu
+:CVSAnnotate
+
+# add instructions for server debug
+# add instructions for web debug
+*/
+
+
+const main = ()=>etask(function*main(){
+});
+
+main();

9ffac5302d39a2251ad4b44a6e5a78907ed8e998
Bright Life <bright@bright.center>
2026-05-17 19:27:28 +0300

    fs.X_OK -> fs.constants.X_OK

--- util/efile.js
+++ util/efile.js
@@ -299,7 +299,7 @@ E.exists = path=>etask(function*exists(){
   return true;
 });
 E.is_exec = path=>etask(function*is_exec(){
-  try { yield etask.nfn_apply(fs.access, [path, fs.X_OK]); }
+  try { yield etask.nfn_apply(fs.access, [path, fs.constants.X_OK]); }
   catch(e){ ef(e); return false; }
   return true;
 });

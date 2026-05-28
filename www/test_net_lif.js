7efa9a7c32b03e937d4f359f2ba95eb647955609
Bright Life <bright@bright.center>
2026-05-28 06:42:45 +0300

    update

--- www/test_net_lif.js
+++ www/test_net_lif.js
@@ -1,8 +1,8 @@
 // http://localhost:4000/?/lif-net//www/test_net_lif.js
-import chai from 'chai';
+import 'chai';
 import mocha from 'mocha';
 mocha.setup({ui: 'bdd', bail: true, timeout: 50000});
 mocha.checkLeaks();
-import net_test from '../net/test.js';
+import '../net/test.js';
 mocha.run();
 

0d927c9ceea798eb086f04b1797d1ee82a5b265d
Bright Life <bright@bright.center>
2026-05-29 08:52:05 +0300

    update

--- www/test_net_lif.js
+++ www/test_net_lif.js
@@ -2,8 +2,20 @@
 import 'lif-kernel/compat/node_env.js';
 import 'chai';
 import mocha from 'mocha';
+function html_elm_frag(html){
+  const template = document.createElement('template');
+  template.innerHTML = html;
+  return template.content.children; // returns HTMLCollection
+}
+
+document.head.append(...html_elm_frag(`<link rel="stylesheet" href="https://unpkg.com/mocha/mocha.css" />`));
+document.body.append(...html_elm_frag(`
+  <h1>LIF Test</h1>
+  <div id=mocha></div>
+`));
+
 mocha.setup({ui: 'bdd', bail: true, timeout: 50000});
 mocha.checkLeaks();
-import '../net/test.js';
+await import('../net/test.js');
 mocha.run();
 
